9dc62d0a34e5492d53a22a57fd79c6663548cc70
Consuelo Sanna <consuelo@MacBook-Pro-di-Consuelo.local>
2020-01-31 12:27:10 +0100

    Create new branch and helloSaga

--- src/redux/saga/sagas.js
+++ src/redux/saga/sagas.js
@@ -1,3 +1,9 @@
+/**
+ * Qui devo inserire tutti generatori di funzione/saga che voglio usare come middleware
+ */
+
 import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
-function* fetchTest(action) {}
+export function* helloSaga() {
+    console.log('Hello Sagas!');
+}
