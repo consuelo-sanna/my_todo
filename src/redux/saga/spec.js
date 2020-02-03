59d8465e068cf0a7e57d814ea4aefbe0e9546649
Consuelo Sanna <consuelo@MacBook-Pro-di-Consuelo.local>
2020-02-03 12:19:08 +0100

    test

--- src/redux/saga/spec.js
+++ src/redux/saga/spec.js
@@ -1,28 +1,22 @@
 import test from 'tape';
 
-import { markAsync, delay } from './sagas';
+import { addAsync, delay } from './sagas';
 import { put, call } from 'redux-saga/effects';
 
 test('markAsync Saga test', assert => {
-    const gen = markAsync();
+    const gen = addAsync();
 
     assert.deepEqual(
         gen.next().value,
-        call(delay, 1000),
+        call(delay, 200),
 
-        'markAsync should return a Promise that will resolve after 1 second'
-    );
-
-    assert.deepEqual(
-        gen.next().value,
-        put({ type: 'MARK_TODO' }),
-        'async saga must dispatch an MARK action'
+        'addAsync should return a Promise that will resolve after 1 second'
     );
 
     assert.deepEqual(
         gen.next(),
         { done: true, value: undefined },
-        'mark saga must be done'
+        'add saga must be done'
     );
     assert.end();
 });
