c6d42328dc7bfcd8768e205e14dd6d97a900eb61
Consuelo Sanna <consuelo@MacBook-Pro-di-Consuelo.local>
2020-02-07 13:03:43 +0100

    Login Logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,42 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import { getUser } from '../redux/selectors/index';
 
+/**
+ * Per il login ecc, riconosco da local storage o ne abuserei?
+ */
 const useStyles = makeStyles(theme => ({
     title: {
         flexGrow: 1,
     },
+    root: {
+        flexGrow: 1,
+    },
 }));
 
-const NavBar = () => {
+const NavBar = props => {
     const classes = useStyles();
+    const user = props.user;
+
+    const guestLinks = <Button color="inherit">Login</Button>;
+    const authLinks = (
+        <Button
+            color="inherit"
+            onClick={() => {
+                localStorage.clear(); //devi anche cancellare user e isAuthenticated da reducer.. fai un dispatch verso reducer, creati l azione ecc
+            }}
+        >
+            Logout
+        </Button>
+    );
+
     return (
-        <div>
-            <AppBar position="static">
+        <div className={classes.root}>
+            <AppBar position="sticky">
                 <Toolbar>
                     <Typography
                         variant="h6"
@@ -23,9 +46,16 @@ const NavBar = () => {
                     >
                         To Do App
                     </Typography>
+
+                    {user ? authLinks : guestLinks}
                 </Toolbar>
             </AppBar>
         </div>
     );
 };
-export default NavBar;
+
+const mapStateToProps = state => ({
+    user: getUser(state),
+});
+
+export default connect(mapStateToProps)(NavBar);
