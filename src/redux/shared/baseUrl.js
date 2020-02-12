cf71845ed97825bdac5726b683662bc2546d0973
Consuelo Sanna <consuelo@MacBook-Pro-di-Consuelo.local>
2020-02-12 15:11:14 +0100

    changed baseUrl

--- src/redux/shared/baseUrl.js
+++ src/redux/shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.REACT_APP_MYURL; //magari piu avanti pensi se mettere anche altri e lo togli da git(gitignore)
+export const baseUrl = process.env.REACT_APP_BASE_URL; //magari piu avanti pensi se mettere anche altri e lo togli da git(gitignore)
